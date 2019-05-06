6322f26d2396c79be2f09ee8949da16cb39fc290
ebrocato <emilybrocato@outlook.com>
2019-05-06 15:03:04 -0600

    refactoring - final commit

--- scripts/store.js
+++ scripts/store.js
@@ -1,13 +1,15 @@
+/* global cuid, Item */
+
 'use strict';
 
 console.log("hello");
 
 
 
-/* global cuid */
+
 
 const store = (function () {
-  
+
   const items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
@@ -15,45 +17,61 @@ const store = (function () {
     { id: cuid(), name: 'bread', checked: false }
   ]
 
-  let hideCheckedItems =  false; //disable-line no-unused-vars
-  let searchTerm =  '';
-  let findById = function(id){
-    items.find(item => item.id === id);
+  let hideCheckedItems = false; //disable-line no-unused-vars
+  let searchTerm = '';
+  const findById = function (id) {
+    return items.find(item => item.id === id);
+
   }
-  
-  
-  let addItem = function(name) {
+
+
+  let addItem = function (name) {
     try {
-      Item.validateName(itemName);
-      items.push(Item.create(itemName))
+      Item.validateName(name);
+      items.push(Item.create(name));
+     
     }
-    catch(e) {
-      console.log('can not add item', e.message)
+    catch (e) {
+      console.log('can not add item', e.message);
     }
-  }
-  
-  
-  let findAndToggleChecked = function(id){
-   let foundItem = this.findById(id);
-     foundItem.checked = !foundItem.checked
-   }
-  
-   let findAndUpdateName = function(id, newName){
-      try{
-        Item.validateName(itemName);
-        let item = this.findById(id);
-        item.name = newName;
-      }
-      catch(e) {
-        console.log('can not update name', e.message);
-      }
+  };
+
+
+  let findAndToggleChecked = function (id) {
+    let foundItem = this.findById(id);
+    console.log(foundItem);
+    foundItem.checked = !foundItem.checked;
+  };
+
+  let findAndUpdateName = function (id, newName) {
+    try {
+      Item.validateName(newName);
+      let item = this.findById(id);
+      item.name = newName;
     }
-  
-    let findAndDelete = function(id) {
-      let index = this.item.findIndex(item => item.id === id);
-      this.items.splice(index, 1);
+    catch (e) {
+      console.log('can not update name', e.message);
     }
-  
+  };
+
+  let findAndDelete = function (id) {
+    let index = this.items.findIndex(item => item.id === id);
+    this.items.splice(index, 1);
+  };
+
+  let toggleCheckedFilter = function() {
+    this.hideCheckedItems = !this.hideCheckedItems;
+
+  };
+
+  let setSearchTerm = function(val) {
+    this.searchTerm = (val);
+
+  };
+
+
+
+
   return {
     items: items,
     hideCheckedItems: hideCheckedItems,
@@ -63,6 +81,8 @@ const store = (function () {
     findAndToggleChecked,
     findAndUpdateName,
     findAndDelete,
+    toggleCheckedFilter,
+    setSearchTerm,
 
   };
 }());
@@ -72,4 +92,3 @@ console.log(store);
 
 
 
-    
